cdbe2fb7ae0c084e9baf9ef1d8950ce4daae1704
Krishnan Shankar <krishnans2006@gmail.com>
2024-06-30 21:15:38 -0400

    Write basic code for OLED display

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include <Arduino.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
 void setup() {
     Serial1.begin(115200);
     Serial1.println("Hello World");
+
+    // Address 0x3D for 128x64
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial1.println(F("SSD1306 allocation failed"));
+        for (;;);
+    }
+
+    delay(2000);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    display.println("Hello, world!!!");
+    display.display();
 }
 
 void loop() {
