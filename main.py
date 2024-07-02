f8c7ea2ec2e72eca351fd5ea6bb3e351ae71dba7
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-01 21:38:49 -0400

    Restructure code for expandability

--- main.py
+++ main.py
@@ -4,30 +4,43 @@ import time
 
 import ssd1306
 
-print("Connecting to WiFi", end="")
+
+WIFI_SSID = "Wokwi-GUEST"
+WIFI_PASS = ""
+
+OLED_WIDTH = 128
+OLED_HEIGHT = 64
+
 
 wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect("Wokwi-GUEST", "")
-
-while not wlan.isconnected():
-    print(".", end="")
-    time.sleep(0.2)
-
-print(f"Done! @ {wlan.ifconfig()[0]}")
-
 
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
+oled = ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c, addr=0x3c)
 
-oled_width = 128
-oled_height = 64
 
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c, addr=0x3c)
+def connect_wifi():
+    print("Connecting to WiFi", end="")
 
-oled.text("Hello, World!", 0, 0)
-oled.text("Hello, World!", 0, 10)
-oled.text("Hello, World!", 0, 20)
-oled.text("Hello, World!", 0, 30)
-oled.show()
+    wlan.active(True)
+    wlan.connect(WIFI_SSID, WIFI_PASS)
 
-time.sleep(2)
+    while not wlan.isconnected():
+        print(".", end="")
+        time.sleep(0.2)
+
+    print(f"Done! @ {wlan.ifconfig()[0]}")
+
+
+def main():
+    connect_wifi()
+    oled.text("Hello, World!", 0, 0)
+    oled.text("Hello, World!", 0, 10)
+    oled.text("Hello, World!", 0, 20)
+    oled.text("Hello, World!", 0, 30)
+    oled.show()
+
+    time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
